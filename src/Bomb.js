7cbdd1241c3a98c50ad6af5a0a73359750e68c39
github username <github email address>
2019-06-12 18:06:02 -0400

    set up condition in functin timer()

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,30 @@
 // your Bomb code here!
+import React from 'react';
+
+export default class Bomb extends React.Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      secondsLeft: props.initialCount
+    }
+  }
+
+  timer = () => {
+    if (this.state.secondsLeft === 0) {
+      return 'Boom!'
+    } else {
+      return (
+        `${this.state.secondsLeft} seconds left before I go boom!`
+      )
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <p>{this.timer()}</p>
+      </div>
+    )
+  }
+}
